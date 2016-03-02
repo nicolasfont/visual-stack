80270c0076b0ed47ded9a61d5cffded2724be890
Alexis King <lexi.lambda@gmail.com>
2016-03-02 11:06:54 -0800

    Extract basic List component from visual stack
    Does not currently support expanding rows.

---
+++ components/List.js
@@ -0,0 +1,56 @@
+import './List.css';
+
+import { map } from 'ramda';
+import React, { PropTypes } from 'react';
+
+export const List = ({ children }) =>
+  <div>{children}</div>;
+
+export const Toolbar = ({ title }) =>
+  <div className="list-toolbar toolbar-widget-header">
+    {title && <span className="list-toolbar-title">{title}</span>}
+  </div>;
+Toolbar.propTypes = {
+  title: PropTypes.string,
+};
+
+const renderHeaderCell = ({ width, alignment, text }) =>
+  <Cell key={text} width={width}>
+    <div className={`list-header-label text-${alignment || 'left'}`}>
+      {text}
+    </div>
+  </Cell>;
+
+export const Header = ({ labels }) =>
+  <div className="list-header">
+    <Row>{map(renderHeaderCell, labels)}</Row>
+  </div>;
+Header.propTypes = {
+  labels: PropTypes.arrayOf(PropTypes.shape({
+    alignment: PropTypes.oneOf(['left', 'center', 'right']),
+    width: PropTypes.number,
+    text: PropTypes.string.isRequired,
+  })).isRequired,
+};
+
+export const Rows = ({ children }) =>
+  <div className="list-rows">
+    {children}
+  </div>;
+
+export const Row = ({ children }) =>
+  <div className="list-row">
+    <div className="container-fluid">
+      <div className="row">
+        {children}
+      </div>
+    </div>
+  </div>;
+
+export const Cell = ({ width, children }) =>
+  <div className={`list-cell col-xs-${width || 1}`}>
+    {children}
+  </div>;
+Cell.propTypes = {
+  width: PropTypes.number,
+};
