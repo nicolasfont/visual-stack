fd7e895bd96c9d9e810c84d8807d36dcb9997146
acave <acave@cj.com>
2016-03-16 21:10:06 -0700

    Adding Spinner component and attempted to have tests for visual-stack components

---
+++ src/components/Spinner.js
@@ -0,0 +1,18 @@
+import React from "react";
+import _ from "underscore";
+import "./spinner.css";
+
+export const Spinner = (props) => {
+    const propsToPassOn = _.omit(props, ["className"]);
+    const className = "spinner spinner-" + (_.isUndefined(props.size))?"small":props.size;
+
+    return (
+        <span className={className} {...propsToPassOn}>
+            <i className="fa fa-spinner cj-i-spin"></i>
+        </span>
+    );
+};
+
+Spinner.propTypes = {
+    "size": React.PropTypes.oneOf(['small', 'large', 'extra-large', 'button'])
+};
