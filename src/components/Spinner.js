1bf2abdb8fcb44a9d863cec56717d2eceded7dae
acave <acave@cj.com>
2016-03-21 17:23:36 -0700

    Fixed eslint errors with Spinner

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,20 +1,20 @@
-import React from "react";
-import _ from "underscore";
-import "./spinner.css";
+import React from 'react';
+import _ from 'underscore';
+import './spinner.css';
 
-const Spinner = (props) => {
-    const propsToPassOn = _.omit(props, ["className"]);
-    const className = "spinner spinner-" + (_.isUndefined(props.size)?"small":props.size);
+const Spinner = props => {
+  const propsToPassOn = _.omit(props, ['className']);
+  const className = 'spinner spinner-' + (_.isUndefined(props.size)?'small':props.size);
 
-    return (
-        <span className={className} {...propsToPassOn}>
-            <i className="fa fa-spinner cj-i-spin"></i>
-        </span>
-    );
+  return (
+     <span className={className} {...propsToPassOn}>
+        <i className="fa fa-spinner cj-i-spin"></i>
+     </span>
+  );
 };
 
 Spinner.propTypes = {
-    "size": React.PropTypes.oneOf(['small', 'large', 'extra-large', 'button'])
+  size: React.PropTypes.oneOf(['small', 'large', 'extra-large', 'button']),
 };
 
 export default Spinner;
