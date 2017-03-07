269d846bcf195df8b0a7df0f0b5200f685dfd555
Franklin Henderson <fhenderson@cj.com>
2017-03-07 12:53:54 -0800

    SlidingPanel toggles onClick of document

--- packages/visual-stack-redux/src/components/SlidingPanel.js
+++ packages/visual-stack-redux/src/components/SlidingPanel.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import R  from 'ramda';
 import { connect } from 'react-redux';
 import {
   SlidingPanel as BaseSlidingPanel,
@@ -14,23 +15,47 @@ export class InternalSlidingPanel extends Component {
   };
   constructor(props) {
     super(props);
+    this.handleClick = e => {
+      const clickInPanel = this.panelRef.contains(e.target);
+      if (!clickInPanel && this.props.active) {
+        this.props.toggleSlidingPanel();
+      }
+    };
+  }
+  componentDidMount() {
+    document.addEventListener('click', this.handleClick, false);
+  }
+  componentWillUnmount() {
+    document.removeEventListener('click', this.handleClick, false);
   }
   render() {
     return (
-      <BaseSlidingPanel active={this.props.active || false} >
-        { this.props.children }
-      </BaseSlidingPanel>
+      <div ref={panel => { this.panelRef = panel; }}>
+        <BaseSlidingPanel
+          active={this.props.active || false} >
+          { this.props.children }
+        </BaseSlidingPanel>
+      </div>
     );
   }
 }
 export const SlidingPanel = connect(
   state => ({ active: state.visualStack.slidingPanel.active }),
+  { toggleSlidingPanel }
 )(InternalSlidingPanel);
 
 export class InternalToggleIcon extends Component {
+  constructor(props) {
+    super(props);
+    const eventSupportsPropagation = R.view(R.lensPath(['nativeEvent', 'stopImmediatePropagation']));
+    this.handleClick = e => {
+      if (eventSupportsPropagation(e)) e.nativeEvent.stopImmediatePropagation();
+      this.props.toggleSlidingPanel();
+    };
+  }
   render() {
     return (
-      <BaseToggleIcon onClick={this.props.toggleSlidingPanel} />
+      <BaseToggleIcon onClick={this.handleClick} />
     );
   }
 }
@@ -41,6 +66,6 @@ export const ToggleIcon = connect(
 )(InternalToggleIcon);
 
 export const SlidingPanelHeader = BaseSlidingPanelHeader;
-export const SlidingPanelSetion = BaseSlidingPanelSection;
+export const SlidingPanelSection = BaseSlidingPanelSection;
 
 
