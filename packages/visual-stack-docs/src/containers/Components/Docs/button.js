799ec888c1fbdf2bcb3ee610e04c93f16d9ca44b
dchong <dchong@cj.com>
2018-10-29 15:20:25 -0700

    Add New Platform buttons

--- packages/visual-stack-docs/src/containers/Components/Docs/button.js
+++ packages/visual-stack-docs/src/containers/Components/Docs/button.js
@@ -2,12 +2,35 @@ import React from 'react';
 import { Panel, Body, Header } from '@cjdev/visual-stack/lib/components/Panel';
 import { Demo, Snippet } from '../../../components/Demo';
 import { Button } from '@cjdev/visual-stack/lib/components/Button';
+import { AccountIcon, CodeIcon, LogoutIcon } from '@cjdev/visual-stack/lib/components/Icons';
 
 export default () =>
     <Demo srcFile="/samples/src/containers/Components/Docs/button.js">
     { snippets => {
       return (
         <div>
+        
+        
+          <Panel>
+            <Header>
+              <b>New</b> Platform Buttons (in progress)
+            </Header>
+            <Body>
+              { /* s3:start */ }
+              <Button type="solid-primary">Solid Primary</Button>
+              <Button type="solid-secondary">Solid Secondary</Button>
+              <Button type="outline-primary">Outline Primary</Button>
+              <Button type="outline-secondary">Outline Secondary</Button>
+              <Button type="rounded-solid">+</Button>
+              <Button type="rounded-outline">–</Button>
+              <Button type="icon"><AccountIcon /></Button>
+              <Button type="text">Text</Button>
+              <Button type="text"><CodeIcon /> Icon and Text</Button>
+              { /* s3:end */ }
+              <Snippet tag="s3" src={snippets} />
+            </Body>
+          </Panel>
+        
           <Panel>
             <Header>
               Default Buttons
@@ -28,11 +51,11 @@ export default () =>
 
           <Panel>
             <Header>
-              Large Buttons
+              Button Sizing
             </Header>
             <Body>
               { /* s1:start */ }
-              <Button type="primary" large={true}>Primary</Button>
+              <Button type="primary" large={true}>Large</Button>
               { /* s1:end */ }
               <Snippet tag="s1" src={snippets} />
             </Body>
