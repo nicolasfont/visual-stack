a8573de1d485f0a792965d42f3946c292d3fab21
dchong <dchong@cj.com>
2018-10-30 14:11:06 -0700

    Initial commit

--- packages/visual-stack-docs/src/containers/Components/Docs/form.js
+++ packages/visual-stack-docs/src/containers/Components/Docs/form.js
@@ -11,6 +11,72 @@ export default () =>
     { snippets => {
       return (
         <div>
+        
+          <Panel>
+            <Header>
+              Compliance - New Issue Form
+            </Header>
+            <Body>
+              <form>
+              	<h1>New Issue</h1>
+              	<div className="container">
+              		<div className="row">
+                		<div className="col-sm-3">
+		                  <Label required={true}>PID</Label>
+		                  <select>
+		                  	<option>Choose PID</option>
+		                  </select>
+                		</div>
+                		<div className="col-sm-3">
+		                  <Label required={true}>Card Name</Label>
+		                  <select>
+		                  	<option>Choose Card</option>
+		                  </select>
+                		</div>
+              		</div>
+              		<div className="row">
+                		<div className="col-sm-3">
+		                  <Label required={true}>Error/Defect Detection</Label>
+		                  <input type="date" />
+                		</div>
+                		<div className="col-sm-3">
+		                  <Label required={true}>Link Cut Date/Time (if applicable)</Label>
+		                  <input type="date" />
+                		</div>
+                		<div className="col-sm-3">
+		                  <Label required={true}>Desolution Date/Time</Label>
+		                  <input type="date" />
+                		</div>
+              		</div>
+              		<div className="row">
+                		<div className="col-sm-6">
+	                		<Label required={true}>Description</Label>
+	                		<textarea>Enter description</textarea>
+                		</div>
+	              	</div>
+              		<div className="row">
+                		<div className="col-sm-6">
+	                		<Label required={true}>Description</Label>
+	                		<input type="text" value="Location of error (URL, app name, etc.)" />
+                		</div>
+	              	</div>
+              		<div className="row">
+                		<div className="col-sm-6">
+	                		<Label required={true}>Error/Defect Detection</Label>
+	                		<textarea>Notes - Root Cause</textarea>
+                		</div>
+	            		</div>
+              		<div className="row">
+                		<div className="col-sm-12">
+                			<button className="vs-btn-d vs-solid-primary-btn">Save</button>
+                			<button className="vs-btn-d vs-text-btn">Cancel</button>
+                		</div>
+              		</div>
+              	</div>
+              </form>
+            </Body>
+          </Panel>
+
           <Panel>
             <Header>
               Default Buttons
@@ -24,7 +90,6 @@ export default () =>
                 </FormGroup>
               </Form>
               { /* s2:end */ }
-              <Snippet tag="s3" src={snippets} />
               <Snippet tag="s2" src={snippets} />
             </Body>
           </Panel>
@@ -46,6 +111,7 @@ export default () =>
               <Snippet tag="s1" src={snippets} />
             </Body>
           </Panel>
+
         </div>
       );
     }}
