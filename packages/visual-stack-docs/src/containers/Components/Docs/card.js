27e6955c24a18114581af55212eb16a5a8e6a2a5
Nedda Amini <namini@cj.com>
2018-11-28 11:51:14 -0800

    Suggested improvements so local classes can be added without clobbering existing styles

--- packages/visual-stack-docs/src/containers/Components/Docs/card.js
+++ packages/visual-stack-docs/src/containers/Components/Docs/card.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Panel, Body, Header } from '@cjdev/visual-stack/lib/components/Panel';
 import { Demo, Snippet } from '../../../components/Demo';
 
+/* s2:start */
 import Card from '@cjdev/visual-stack/lib/components/Card';
+/* s2:end */
 
 export default () =>
   <Demo srcFile="/samples/src/containers/Components/Docs/card.js">
@@ -14,8 +16,9 @@ export default () =>
               Spinner
             </Header>
             <Body>
+              <Snippet tag="s2" src={snippets} />
               { /* s1:start */ }
-              <Card href="https://cj.com">Go to cj.com</Card>
+              <Card href="https://cj.com" styles="additional-classes">Go to cj.com</Card>
               { /* s1:end */ }
               <Snippet tag="s1" src={snippets} />
             </Body>
--- packages/visual-stack/src/components/Card/index.js
+++ packages/visual-stack/src/components/Card/index.js
@@ -6,5 +6,5 @@ import * as R from 'ramda';
 const isPropEmpty = (prop) => (R.isNil(prop) || R.isEmpty(prop));
 
 export default (props) => (isPropEmpty(props.href)) ? 
-  <div className="vs-card" {...props}>{props.children}</div> :
-  <a className="vs-card-clickable" {...props}>{props.children}</a>;
+  <div className={`vs-card ${ props.styling ? props.styling : '' }`} {...props}>{props.children}</div> :
+  <a className={`vs-card ${ props.styling ? props.styling : '' }`} {...props}>{props.children}</a>;
