b74ad7a10d9df05abb1d90e76fbea2dcaac865bf
dchong <dchong@cj.com>
2018-12-27 16:13:10 -0800

    Merge branch 'ui-structural-metrics' of https://github.com/cjdev/visual-stack into ui-structural-metrics
    # Conflicts:
    #	packages/visual-stack/src/components/SideNav/SideNav.css

---
+++ packages/visual-stack-docs/src/containers/Components/Docs/card.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Panel, Body, Header } from '@cjdev/visual-stack/lib/components/Panel';
+import { Demo, Snippet } from '../../../components/Demo';
+
+/* s2:start */
+import Card from '@cjdev/visual-stack/lib/components/Card';
+/* s2:end */
+
+export default () =>
+  <Demo srcFile="/samples/src/containers/Components/Docs/card.js">
+    { snippets => {
+      return (
+        <div>
+          <Panel>
+            <Header>
+              Spinner
+            </Header>
+            <Body>
+              <Snippet tag="s2" src={snippets} />
+              { /* s1:start */ }
+              <Card href="https://cj.com" className="additional-classes">Go to cj.com</Card>
+              { /* s1:end */ }
+              <Snippet tag="s1" src={snippets} />
+            </Body>
+          </Panel>
+        </div>
+      );
+    }}
+  </Demo>;
