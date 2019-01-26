9fb83b01895c25f99c5933d2d3866fd1abcea3fc
jshah <jshah@cj.com>
2019-01-26 13:12:03 -0800

    more docs for form components and reorganized a bit

--- packages/visual-stack-docs/src/containers/Components/Docs/form.js
+++ packages/visual-stack-docs/src/containers/Components/Docs/form.js
@@ -6,18 +6,33 @@ import { Demo, Snippet } from '../../../components/Demo';
 import { Form, FormGroup, Label, Input, Legend, InputField, LabeledComponent, ChoiceField } from '@cjdev/visual-stack/lib/components/Form';
 /* s3:end */
 
-/* s7:start */
+/* s11:start */
 // Styles used above
 const inlineOtherTextBoxStyle = {
   display: "inline",
   marginRight: "8px"
 };
-
 const makeTextBoxSmallerStyle = {
   width: "50px",
-  display: "inline"
+  display: "inline",
+  marginRight: "8px"
+
 };
-/* s7:end */
+const gridItUp ={
+    display: "grid",
+    gridTemplateColumns: "3fr 3fr 3fr",
+    gridColumnGap: "8px"
+};
+/* s11:end */
+
+const PanelComponent =({header, children}) => (
+    <Panel>
+        {header ? <Header>{header}</Header> : null}
+        <Body>
+            {children}
+        </Body>
+    </Panel>
+);
 
 export default () =>
     <Demo srcFile="/samples/src/containers/Components/Docs/form.js">
@@ -60,87 +75,132 @@ export default () =>
             </Body>
           </Panel>
 
-          <Panel>
-            <Header>
-              InputField Labeled Components
-            </Header>
-            <Body>
+          <PanelComponent header="InputField Labeled Components">
+            <PanelComponent>
               { /* s4:start */ }
-              <Form vertical={false}>
+              <Form>
                   <InputField label="Some Label"
                               help="this is some field"
-                              placeholder="this is where you should enter stuff"/>
-
-                  <InputField label="Another Label (Does not display help when error)"
-                              value="value with validation error"
-                              error="Please enter correct value"
-                              help="this will not display when error is present"/>
-
-                  <InputField label="Not required"
-                              optional={true}
-                              optionalLabel="Optional"
-                              help="this is some field"
-                              placeholder="this is where you should enter stuff"/>
+                              placeholder="placeholder text"/>
               </Form>
               { /* s4:end */ }
               <Snippet tag="s4" src={snippets} />
-            </Body>
-          </Panel>
-
-          <Panel>
-            <Header>
-              Labeled Components
-            </Header>
-            <Body>
-            { /* s5:start */ }
-            <Form vertical={false}>
-              <LabeledComponent label="Simple radio group"
-                          help="A group of radio buttons">
-                <ChoiceField name="radio1" label="Radio 1" value="radio1" checked={true}/>
-                <ChoiceField name="radio1" label="Radio 2" value="radio2"/>
-              </LabeledComponent>
-
-              <LabeledComponent label="Radio group with error"
-                                help="A group of radio buttons"
-                                error="Somehow something went wrong">
-                <ChoiceField name="radio2" label="Radio 1" value="radio1"/>
-                <ChoiceField name="radio2" label="Radio 2" value="radio2"/>
-              </LabeledComponent>
-
-              <LabeledComponent label="Checkbox group with optional label"
-                                help="A group of Checkboxes"
+            </PanelComponent>
+            <PanelComponent>
+                { /* s5:start */ }
+              <Form>
+                  <InputField label="Another Label (Does not display help when error)"
+                              value="value with validation error"
+                              error="Please enter correct value"
+                              help="help will be replaced with error when present"/>
+              </Form>
+                { /* s5:end */ }
+              <Snippet tag="s5" src={snippets} />
+            </PanelComponent>
+            <PanelComponent>
+              { /* s6:start */ }
+              <Form>
+                <InputField label="Not required"
                                 optional={true}
-                                optionalLabel="Optional">
-                <ChoiceField name="checkbox1" type="checkbox" label="Checkbox 1" value="checkbox1"/>
-                <ChoiceField name="checkbox2" type="checkbox" label="Checkbox 2" value="checkbox2"/>
-              </LabeledComponent>
-            </Form>
-            { /* s5:end */ }
-            <Snippet tag="s5" src={snippets} />
-            </Body>
-          </Panel>
+                                optionalLabel="Optional"
+                                help="this is some field"
+                                placeholder="this is where you should enter stuff"/>
+              </Form>
+              { /* s6:end */ }
+              <Snippet tag="s6" src={snippets} />
+            </PanelComponent>
+          </PanelComponent>
 
-          <Panel>
-            <Header>
-              Complicated Labeled Components
-            </Header>
-            <Body>
-            { /* s6:start */ }
-            <Form vertical={false}>
-              <LabeledComponent label="Some complicated stuff goin' on here"
-                                help="A group of radio buttons with complications">
-                <ChoiceField label="To be" value="complicated"/>
-                <div style={inlineOtherTextBoxStyle}>
-                  <ChoiceField label="Not to be" value="simple" style={inlineOtherTextBoxStyle}/>
-                  <Input name="numberOfReferrals.days" style={makeTextBoxSmallerStyle}/> <Label>something</Label>
-                </div>
-              </LabeledComponent>
+          <PanelComponent header="Labeled Components">
+
+            <PanelComponent>
+                { /* s7:start */ }
+                <Form>
+                  <LabeledComponent label="Simple radio group"
+                              help="A group of radio buttons">
+                    <ChoiceField name="radio1" label="Radio 1" value="radio1" checked={true}/>
+                    <ChoiceField name="radio1" label="Radio 2" value="radio2"/>
+                  </LabeledComponent>
+                </Form>
+                  { /* s7:end */ }
+                <Snippet tag="s7" src={snippets} />
+            </PanelComponent>
+
+            <PanelComponent>
+              {/* s8:start */}
+              <Form>
+                  <LabeledComponent label="Radio group with error"
+                                    help="A group of radio buttons"
+                                    error="Somehow something went wrong">
+                      <ChoiceField name="radio2" label="Radio 1" value="radio1"/>
+                      <ChoiceField name="radio2" label="Radio 2" value="radio2"/>
+                  </LabeledComponent>
+              </Form>
+              {/* s8:end */}
+              <Snippet tag="s8" src={snippets}/>
+            </PanelComponent>
+
+            <PanelComponent>
+              {/* s9:start */}
+              <Form>
+                  <LabeledComponent label="Checkbox group with optional label"
+                                    help="A group of Checkboxes"
+                                    optional={true}
+                                    optionalLabel="Optional">
+                      <ChoiceField name="checkbox1" type="checkbox" label="Checkbox 1" value="checkbox1"/>
+                      <ChoiceField name="checkbox2" type="checkbox" label="Checkbox 2" value="checkbox2"/>
+                  </LabeledComponent>
+              </Form>
+              {/* s9:end */}
+              <Snippet tag="s9" src={snippets}/>
+            </PanelComponent>
+
+          </PanelComponent>
+
+        <PanelComponent header="Complicated Labeled Components">
+            {/* s10:start */}
+            <Form>
+                <LabeledComponent label="Some complicated stuff goin' on here"
+                                  help="A group of radio buttons with complications">
+                    <ChoiceField label="To be" value="complicated"/>
+                    <div style={inlineOtherTextBoxStyle}>
+                        <ChoiceField label="Not to be" value="simple" style={inlineOtherTextBoxStyle}/>
+                        <Input name="numberOfReferrals.days" style={makeTextBoxSmallerStyle}/>
+                        <Label>something</Label>
+                    </div>
+                </LabeledComponent>
             </Form>
-            { /* s6:end */ }
-            <Snippet tag="s6" src={snippets} />
-            <Snippet tag="s7" src={snippets} />
-            </Body>
-          </Panel>
+            {/* s10:end */}
+            <Snippet tag="s10" src={snippets}/>
+            <Snippet tag="s11" src={snippets}/>
+        </PanelComponent>
+        <PanelComponent header="All together with some grid stuff">
+            {/* s12:start */}
+            <Form>
+                <InputField name="name" label="Enter Name" help="Enter first name and last name" placeholder="First Last"/>
+                <InputField name="country" label="Country" optional={true} optionalLabel="Optional"
+                            value="Degobah" error="Enter valid country name on planet Earth" help="Enter Country"/>
+                <div style={gridItUp}>
+                    <LabeledComponent name="vacationDays" label="# of vacation days" help="Enter vacation rules">
+                        <ChoiceField label="Unlimited" value="unlimited"/>
+                        <div style={inlineOtherTextBoxStyle}>
+                            <ChoiceField label="Limit to" value="limitTo" style={inlineOtherTextBoxStyle}/>
+                            <Input name="days" style={makeTextBoxSmallerStyle}/>
+                            <Label>days</Label>
+                        </div>
+                    </LabeledComponent>
+                    <LabeledComponent name="vacationtypes" label="Vacation Type" help="Choose all applicable"
+                                      optional={true} optionalLabel="Optional">
+                        <ChoiceField name="vacationtypes.cruise" type="checkbox" label="Cruise" value="cruise"/>
+                        <ChoiceField name="vacationtypes.hiking" type="checkbox" label="Hiking" value="hiking"/>
+                    </LabeledComponent>
+                    <div/>
+                </div>
+            </Form>
+            {/* s12:end */}
+            <Snippet tag="s12" src={snippets}/>
+            <Snippet tag="s11" src={snippets}/>
+        </PanelComponent>
         </div>
       );
     }}
