3e9cd969e1da9a05d596376fed9ef3d3a3a3d493
Franklin Henderson <fhenderson@cj.com>
2019-02-06 07:58:26 -0800

    setup v-s-r jest

---
+++ packages/visual-stack-redux/src/setupTests.js
@@ -0,0 +1,24 @@
+const jsdom = require("jsdom");
+const doc = jsdom.jsdom("");
+const window = doc.defaultView;
+
+
+function copyProps(src, target) {
+  Object.defineProperties(target, {
+    ...Object.getOwnPropertyDescriptors(src),
+    ...Object.getOwnPropertyDescriptors(target),
+  });
+}
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js',
+};
+global.requestAnimationFrame = function (callback) {
+  return setTimeout(callback, 0);
+};
+global.cancelAnimationFrame = function (id) {
+  clearTimeout(id);
+};
+copyProps(window, global);
