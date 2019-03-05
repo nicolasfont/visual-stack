f6e6bd9212c6396ab31df33b3e6a24687db431ba
Mircea Nedelcu <mnedelcu@cj.com>
2019-03-05 11:41:44 -0800

    styling for collapsible docs

--- packages/visual-stack-docs/src/containers/Components/Docs/collapsiblepanel.js
+++ packages/visual-stack-docs/src/containers/Components/Docs/collapsiblepanel.js
@@ -1,15 +1,17 @@
 import * as React from 'react';
-import {Body, Panel} from "@cjdev/visual-stack/lib/components/Panel";
+import {Body, Panel, Header} from "@cjdev/visual-stack/lib/components/Panel";
 import {Button} from "@cjdev/visual-stack/lib/components/Button";
 import ChevronRightIcon from 'mdi-react/ChevronRightIcon';
 import ChevronDownIcon from 'mdi-react/ChevronDownIcon';
 import "./collapsiblepanel.css";
+import {FieldContent, Input, Label, ChoiceInput, Field, Form} from "@cjdev/visual-stack/lib/components/Form";
+import {Demo, Snippet} from "../../../components/Demo";
 
 class CollapsiblePanel extends React.Component {
   constructor(props) {
     super(props);
 
-    const collapsed = props.collapsed === undefined ? true : props.collapsed;
+    const collapsed = props.initiallyCollapsed === undefined ? true : props.initiallyCollapsed;
     this.state = {collapsed};
 
     this.toggleCollapsed = this.toggleCollapsed.bind(this);
@@ -21,7 +23,7 @@ class CollapsiblePanel extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="vs-collapsible-panel">
         <div className="vs-collapsible-panel-header">
           <Button type="icon" onClick={this.toggleCollapsed}>
             {this.state.collapsed
@@ -46,18 +48,56 @@ class CollapsiblePanel extends React.Component {
 
 export default () => {
   return (
-    <Panel>
-      <Body>
-      <CollapsiblePanel title={<h3 className="inline-remove-margin">Collapsible Panel title</h3>}>
-        <div>Collapsible Panel item</div>
-        <div>Collapsible Panel item 2</div>
-      </CollapsiblePanel>
+    <Demo srcFile="/samples/src/containers/Components/Docs/collapsiblepanel.js">
+      {snippets => (
+        <Panel>
+          <Header>Collapsible Panels</Header>
+          <Body paddingSize="none">
+          {/* s1:start */}
+          <CollapsiblePanel title={<h3 className="inline-remove-margin">Collapsible Panel 1</h3>}
+                            initiallyCollapsed={false}>
+            <Field label="# of vacation days" help="Enter vacation rules">
+              <FieldContent>
+                <ChoiceInput name="vacationDays" label="Unlimited" value="unlimited"/>
+                <div>
+                  <ChoiceInput name="vacationDays" label="Limit to" value="limitTo"
+                               className="inline-other-text-box-style"/>
+                  <Input name="days" className="make-text-box-smaller-style"/>
+                  <Label>days</Label>
+                </div>
+              </FieldContent>
+            </Field>
+            <Field name="vacationtypes" label="Vacation Type" help="Choose all applicable"
+                   optional={true} optionalLabel="Optional">
+              <FieldContent>
+                <ChoiceInput name="vacationtypes.cruise" type="checkbox" label="Cruise" value="cruise"/>
+                <ChoiceInput name="vacationtypes.hiking" type="checkbox" label="Hiking" value="hiking"/>
+              </FieldContent>
+            </Field>
+            <Snippet tag="s1" src={snippets}/>
+          </CollapsiblePanel>
+          {/* s1:end */}
 
-      <CollapsiblePanel title={<h3 className="inline-remove-margin">Collapsible Panel 2</h3>}>
-        <div>Collapsible Panel item</div>
-        <div>Collapsible Panel item 2</div>
-      </CollapsiblePanel>
-      </Body>
-    </Panel>
+          {/* s2:start */}
+          <CollapsiblePanel title="Collapsible Panel 2">
+            <div className="summary-content">
+              <Label>Default commissioning</Label>
+              <Label fontWeight="normal">3%</Label>
+            </div>
+            <div className="summary-content">
+              <Label>Situation 1</Label>
+              <Label fontWeight="normal">4%</Label>
+            </div>
+            <div className="summary-content">
+              <Label>Situation 2</Label>
+              <Label fontWeight="normal">5%</Label>
+            </div>
+            <Snippet tag="s2" src={snippets}/>
+          </CollapsiblePanel>
+          {/* s2:end */}
+          </Body>
+        </Panel>
+      )}
+    </Demo>
   );
 };
