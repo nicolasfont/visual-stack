bd91ff6d1d7179256d8b56f9b0e7fc439ef3f228
Hoberg, Kyle <khoberg@cj.com>
2019-03-19 16:37:03 -0700

    Remove extra props and namespace css styles

--- packages/visual-stack/src/components/Select/index.js
+++ packages/visual-stack/src/components/Select/index.js
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactSelect from 'react-select';
 import './Select.css';
 
-const Select = ({name, className, error, value, onChange, onBlur, disabled, ...otherProps}) => (
-  <ReactSelect className={(error ? "input-error" : "") + " default-react-select " + (className ? className : "")}
-               classNamePrefix={"react-select"}
-               name={name}
-               value={value}
-               onChange={onChange}
+const Select = ({className, error, disabled, ...otherProps}) => (
+  <ReactSelect className={(error ? "vs-input-error" : "") + " vs-default-react-select " + (className ? className : "")}
+               classNamePrefix={"vs-react-select"}
                isDisabled={disabled}
-               onBlur={onBlur}
                {...otherProps}/>
 );
 
