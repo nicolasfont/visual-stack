2a87330ccaac71051aa8de99dd4b987715286f83
Hoberg, Kyle <khoberg@cj.com>
2019-03-19 16:40:31 -0700

    Use classnames for class names on select

--- packages/visual-stack/src/components/Select/index.js
+++ packages/visual-stack/src/components/Select/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactSelect from 'react-select';
+import cn from 'classnames';
 import './Select.css';
 
 const Select = ({className, error, disabled, ...otherProps}) => (
-  <ReactSelect className={(error ? "vs-input-error" : "") + " vs-default-react-select " + (className ? className : "")}
+  <ReactSelect className={cn({"vs-input-error": error}, "vs-default-react-select", className)}
                classNamePrefix={"vs-react-select"}
                isDisabled={disabled}
                {...otherProps}/>
