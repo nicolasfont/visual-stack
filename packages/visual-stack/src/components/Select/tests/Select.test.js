158f469a3d24f8e5858ea417248706ae0c2095ca
Nedda Amini <nedda.amini@gmail.com>
2019-03-19 17:18:02 -0700

    Moved test to be co-located with Select component

--- packages/visual-stack/src/components/Select/tests/Select.test.js
+++ packages/visual-stack/src/components/Select/tests/Select.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Select from '../Select';
+import Select from '../index';
 import Enzyme, {shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
