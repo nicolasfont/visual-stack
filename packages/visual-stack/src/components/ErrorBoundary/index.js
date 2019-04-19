0544aa3836483d71be52b5a8106d71cfb336b773
Edward Langley <elangley@cj.com>
2019-04-18 17:19:18 -0700

    Make ErrorBoundary more flexible

--- packages/visual-stack/src/components/ErrorBoundary/index.js
+++ packages/visual-stack/src/components/ErrorBoundary/index.js
@@ -1,21 +1,54 @@
+/* eslint-disable no-console */
 import React from 'react';
+import PropTypes from 'prop-types';
 
-class ErrorBoundary extends React.Component {
-  state = {
-    error: false,
-  };
+export class ErrorBoundary extends React.Component {
+  constructor() {
+    super();
+    this.state = { ok: true };
+    this.resetState = this.resetState.bind(this);
+  }
 
-  static getDerivedStateFromError() {
-    return { error: true };
+  resetState() {
+    this.setState(() => ({ ok: true }));
+  }
+
+  componentDidCatch(err, _) {
+    const ok = this.props.onError(err, this.props.recoveries, this.resetState);
+    if (!ok) {
+      this.setState(() => ({ ok }));
+    }
   }
 
   render() {
-    if (this.state.error) {
-      return <h1>Something went wrong.</h1>;
-    } else {
+    if (this.state.ok) {
       return this.props.children;
+    } else {
+      return this.props.errorRenderer();
     }
   }
+
+  static propTypes = {
+    children: PropTypes.node,
+    onError: PropTypes.func,
+    errorRenderer: PropTypes.func,
+    recoveries: PropTypes.objectOf(PropTypes.func),
+  };
+
+  static defaultProps = {
+    onError(err, recoveries) {
+      recoveries.squash(err);
+    },
+    errorRenderer() {
+      return <div>Something went wrong.</div>;
+    },
+    recoveries: {
+      squash() {},
+      log(err) {
+        console.error(`[Error]: ${err}`);
+      },
+    },
+  };
 }
 
 export const withErrorBoundary = MyComponent => {
--- packages/visual-stack/src/components/ErrorBoundary/tests/index.test.js
+++ packages/visual-stack/src/components/ErrorBoundary/tests/index.test.js
@@ -1,28 +1,98 @@
 import React from 'react';
 import Enzyme, { mount } from 'enzyme';
-import { withErrorBoundary } from '../';
+import * as uut from '../';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-const suppressError = () => {
-  // eslint-disable-next-line no-console
-  console.error = () => {};
-};
+const BadComponent = _ => null;
 
-describe('ErrorBoundary', () => {
+describe('withErrorBoundary', () => {
   it('should render an error message', () => {
-    suppressError();
-    const BadComponent = () => <div>{undefined.map}</div>;
-    const ErrorBoundedComponent = withErrorBoundary(BadComponent);
+    const ErrorBoundedComponent = uut.withErrorBoundary(BadComponent);
     const wrapper = mount(<ErrorBoundedComponent />);
+    wrapper.find(BadComponent).simulateError(new Error('fail!'));
+
     expect(wrapper.text()).toEqual('Something went wrong.');
   });
 
   it('should render the component if there is no exception', () => {
     const GoodComponent = () => <div>Good</div>;
-    const ErrorBoundedComponent = withErrorBoundary(GoodComponent);
+    const ErrorBoundedComponent = uut.withErrorBoundary(GoodComponent);
     const wrapper = mount(<ErrorBoundedComponent />);
+
     expect(wrapper.text()).toEqual('Good');
+  });
+});
+
+describe('ErrorBoundary', () => {
+  test('works in happy path', () => {
+    const wrapper = mount(
+      <uut.ErrorBoundary>
+        <div id="expected" />
+      </uut.ErrorBoundary>
+    );
+
+    expect(wrapper.find('#expected')).toHaveLength(1);
+  });
+
+  test('uses error renderer', () => {
+    const wrapper = mount(
+      <uut.ErrorBoundary errorRenderer={() => <div id="expected" />}>
+        <BadComponent />
+      </uut.ErrorBoundary>
+    );
+
+    wrapper.find(BadComponent).simulateError(new Error('fail!'));
+
+    expect(wrapper.find('#expected')).toHaveLength(1);
+  });
+
+  describe('onError', () => {
+    test('passes error to onError', () => {
+      expect.hasAssertions();
+      const err = new Error('fail');
+
+      let spy = false;
+      const wrapper = mount(
+        <uut.ErrorBoundary
+          onError={err => {
+            spy = err;
+          }}
+        >
+          <BadComponent {...{ err }} />
+        </uut.ErrorBoundary>
+      );
+
+      wrapper.find(BadComponent).simulateError(err);
+
+      expect(spy).toEqual(err);
+    });
+
+    test('uses recoveries', () => {
+      expect.hasAssertions();
+      const err = new Error('fail');
+
+      let spy = false;
+
+      const wrapper = mount(
+        <uut.ErrorBoundary
+          onError={(err, recoveries) => {
+            recoveries.recover(err);
+          }}
+          recoveries={{
+            recover(err) {
+              spy = err;
+            },
+          }}
+        >
+          <BadComponent />
+        </uut.ErrorBoundary>
+      );
+
+      wrapper.find(BadComponent).simulateError(err);
+
+      expect(spy).toEqual(err);
+    });
   });
 });
