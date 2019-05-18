6ebcbf7ec023048829bf34271d88de29b1d9a95d
Edward Langley <elangley@cj.com>
2019-05-17 22:22:08 -0700

    Prettify and highlight snippets

--- packages/visual-stack-docs/src/components/Demo/index.js
+++ packages/visual-stack-docs/src/components/Demo/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import * as R from 'ramda';
 import { parse, trimLeadingWhiteSpace } from './snippetParser';
 import './styles.css';
+import Highlight, { defaultProps } from 'prism-react-renderer';
+import vsDark from 'prism-react-renderer/themes/vsDark';
+import { format } from 'prettier/standalone';
+import jsParser from 'prettier/parser-babylon';
 
 export const InlineSnippet = ({ tag, src }) => {
   const srcTag = R.view(R.lensPath([tag]))(src);
@@ -18,7 +22,27 @@ export const Snippet = ({ tag, src }) => {
   if (!srcTag) return null;
   return (
     <div className="snippet">
-      <pre>{trimLeadingWhiteSpace(src[tag]).join('\n')}</pre>
+      <Highlight
+        {...defaultProps}
+        code={format(src[tag].join('\n'), {
+          parser: 'babel',
+          plugins: [jsParser],
+        })}
+        language="jsx"
+        theme={vsDark}
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className} style={style}>
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
     </div>
   );
 };
