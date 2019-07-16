2d66837923360196205fde46ee74c5c6f32f74e9
zhuochengyu <zyu@cj.com>
2019-07-15 17:01:39 -0700

    add redux data table component

---
+++ packages/visual-stack-redux/src/components/DataTable/index.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import { DataTable as VSDataTable } from '@cjdev/visual-stack/lib/components/Table/DataTable';
+import { connect } from 'react-redux';
+import {
+  initializeDataTable,
+  setDataTablePage,
+  setDataTableSortingOption,
+  selectDataTable,
+} from '../../actions';
+import * as R from 'ramda';
+import PropTypes from 'prop-types';
+
+export class DataTablePure extends Component {
+  componentDidMount() {
+    const initialPayload = R.pick(['id', 'data'])(this.props);
+    this.props.initializeDataTable(initialPayload);
+  }
+
+  render() {
+    const {
+      sortable,
+      columns,
+      caption,
+      dataTable,
+      pagination,
+      id,
+      setDataTablePage,
+      setDataTableSortingOption,
+    } = this.props;
+    return (
+      <div>
+        <VSDataTable
+          caption={caption}
+          sortable={sortable}
+          pagination={pagination}
+          data={dataTable.data}
+          columns={columns}
+          page={dataTable.pagination.page}
+          rowsPerPage={dataTable.pagination.rowsPerPage}
+          sortingOption={dataTable.sortingOption}
+          onPageChange={pagination => {
+            setDataTablePage({ id, pagination });
+          }}
+          onSort={({ data, sortingOption }) => {
+            setDataTableSortingOption({ id, data, sortingOption });
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+DataTablePure.propTypes = {
+  caption: PropTypes.string,
+  sortable: PropTypes.bool,
+  pagination: PropTypes.bool,
+  data: PropTypes.array,
+  columns: PropTypes.array,
+  dataTable: PropTypes.shape({
+    data: PropTypes.array,
+    sortingOption: PropTypes.shape({
+      label: PropTypes.string,
+      order: PropTypes.string,
+    }),
+    pagination: PropTypes.shape({
+      page: PropTypes.number,
+      rowsPerPage: PropTypes.number,
+    }),
+  }),
+  initializeDataTable: PropTypes.func.isRequired,
+  setDataTablePage: PropTypes.func,
+  setDataTableSortingOption: PropTypes.func,
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  dataTable: selectDataTable(ownProps.id)(state),
+});
+
+const mapDispatchToProps = {
+  initializeDataTable,
+  setDataTablePage,
+  setDataTableSortingOption,
+};
+
+export const DataTable = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DataTablePure);
---
+++ packages/visual-stack-redux/src/components/DataTable/tests/index.test.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import { DataTablePure } from '../';
+import { DataTable as VSDataTable } from '@cjdev/visual-stack/lib/components/Table/DataTable';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('DataTablePure', () => {
+  it('should initialize data table when first mounted', () => {
+    const initializeDataTable = jest.fn();
+    const id = '1';
+    const data = [{ id: 1 }];
+    shallow(
+      <DataTablePure
+        id={id}
+        data={data}
+        dataTable={{ pagination: {} }}
+        initializeDataTable={initializeDataTable}
+      />
+    );
+    expect(initializeDataTable.mock.calls).toEqual([
+      [
+        {
+          id,
+          data,
+        },
+      ],
+    ]);
+  });
+
+  it('should call setDataTablePage when changing page from VSDataTable', () => {
+    const id = '1';
+    const setDataTablePage = jest.fn();
+    const paginationPayload = { page: 2, rowsPerPage: 50 };
+    const wrapper = shallow(
+      <DataTablePure
+        id={id}
+        dataTable={{ pagination: {} }}
+        initializeDataTable={() => {}}
+        setDataTablePage={setDataTablePage}
+      />
+    );
+    const { onPageChange } = wrapper.find(VSDataTable).props();
+    onPageChange(paginationPayload);
+    expect(setDataTablePage.mock.calls).toEqual([
+      [
+        {
+          id,
+          pagination: paginationPayload,
+        },
+      ],
+    ]);
+  });
+
+  it('should call setDataTableSortingOption when sorting from VSDataTable', () => {
+    const id = '1';
+    const setDataTableSortingOption = jest.fn();
+    const sortingPayload = {
+      sortingOption: { label: 'id', order: 'ASCENDING' },
+      data: [{ id: 1 }],
+    };
+    const wrapper = shallow(
+      <DataTablePure
+        id={id}
+        dataTable={{ pagination: {} }}
+        initializeDataTable={() => {}}
+        setDataTableSortingOption={setDataTableSortingOption}
+      />
+    );
+    const { onSort } = wrapper.find(VSDataTable).props();
+    onSort(sortingPayload);
+    expect(setDataTableSortingOption.mock.calls).toEqual([
+      [
+        {
+          id,
+          ...sortingPayload,
+        },
+      ],
+    ]);
+  });
+
+  it('should wire the correct props', () => {
+    const id = '1';
+    const data = [{ id: 1 }];
+    const initializeDataTable = jest.fn();
+    const page = 1;
+    const columns = [{ label: 'id' }];
+    const rowsPerPage = 10;
+    const sortable = false;
+    const pagination = true;
+    const sortingOption = {
+      label: 'id',
+      order: 'ASCENDING',
+    };
+    const caption = 'I am a table';
+    const dataTable = {
+      data,
+      pagination: {
+        page,
+        rowsPerPage,
+      },
+      sortingOption,
+    };
+    const wrapper = shallow(
+      <DataTablePure
+        caption={caption}
+        columns={columns}
+        id={id}
+        sortable={sortable}
+        pagination={pagination}
+        data={[]}
+        dataTable={{ pagination: {} }}
+        initializeDataTable={initializeDataTable}
+        dataTable={dataTable}
+      />
+    );
+    const props = wrapper.find(VSDataTable).props();
+    expect(props.caption).toEqual(caption);
+    expect(props.columns).toEqual(columns);
+    expect(props.sortable).toEqual(sortable);
+    expect(props.pagination).toEqual(pagination);
+    expect(props.data).toEqual(dataTable.data);
+    expect(props.page).toEqual(dataTable.pagination.page);
+    expect(props.rowsPerPage).toEqual(dataTable.pagination.rowsPerPage);
+    expect(props.sortingOption).toEqual(dataTable.sortingOption);
+  });
+});
