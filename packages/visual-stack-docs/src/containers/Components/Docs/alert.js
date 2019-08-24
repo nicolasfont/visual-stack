faaecfc652500eb864347b3bfe332187e8f33eb2
Chong <dchong@wl-dchong-mac.lan>
2019-08-23 17:27:50 -0700

    Update alert styling

--- packages/visual-stack-docs/src/containers/Components/Docs/alert.js
+++ packages/visual-stack-docs/src/containers/Components/Docs/alert.js
@@ -70,7 +70,7 @@ const InfoAlertWithIconCloseButton = createButton({
   type: 'info',
   closeType: 'icon',
   closeButtonText: null,
-  children: 'Please review the form. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  children: '<h3>Please review the form</h3> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>',
 });
 
 const InfoAlertWithButtonCloseButton = createButton({
