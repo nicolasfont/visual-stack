55eabf93eb000baea74b3e08b56aa04806be75cc
Hoberg, Kyle <khoberg@cj.com>
2019-11-04 11:30:52 -0800

    Give popover some props

--- packages/visual-stack/src/components/Popover/index.js
+++ packages/visual-stack/src/components/Popover/index.js
@@ -2,24 +2,22 @@ import React from 'react';
 import {Manager, Popper, Reference} from 'react-popper';
 import "./Popover.css";
 
-const Popover = () => {
+const Popover = ({shown, content, children, placement = "bottom"}) => {
   return (
     <Manager>
       <Reference>
-        {({ ref }) => (
-          <button type="button" ref={ref} style={{"margin": "300px"}}>
-            Reference element
-          </button>
+        {({ref}) => (
+          <span ref={ref} className="vs-popover-reference">
+            {children}
+          </span>
         )}
       </Reference>
-      <Popper placement={"bottom"}>
-        {({ ref, style, placement, arrowProps }) => (
-          <>
-            <div className="vs-popover-container" ref={ref} style={style} data-placement={placement}>
-              Popper element<br/>test
-              <span className="vs-popover-arrow" ref={arrowProps.ref} style={arrowProps.style} data-placement={placement} />
-            </div>
-          </>
+      <Popper placement={placement}>
+        {({ref, style, placement, arrowProps}) => (
+          <div className="vs-popover-container" ref={ref} style={style} data-placement={placement} hidden={!shown}>
+            {content}
+            <span className="vs-popover-arrow" ref={arrowProps.ref} style={arrowProps.style} data-placement={placement}/>
+          </div>
         )}
       </Popper>
     </Manager>
--- packages/visual-stack/src/components/Popover/tests/Popover.test.js
+++ packages/visual-stack/src/components/Popover/tests/Popover.test.js
@@ -1,7 +1,59 @@
 import React from 'react';
+import Popover from "../index";
+import Enzyme, {mount} from 'enzyme';
+import {Popper} from 'react-popper';
+
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe("Popover", () => {
-  it("should have a test", () => {
+  it("should render when told to show and default placement bottom", () => {
+    const wrapper = mount(
+      <Popover shown={true} />
+    );
 
+    expect(wrapper.find(".vs-popover-container").prop("hidden")).toBe(false);
+    expect(wrapper.find(Popper).prop("placement")).toBe("bottom");
+  });
+
+  it("should be hidden when not shown", () => {
+    const wrapper = mount(
+      <Popover shown={false} />
+    );
+
+    expect(wrapper.find(".vs-popover-container").prop("hidden")).toBe(true);
+  });
+
+  it("should accept a placement prop to control default placement", () => {
+    const wrapper = mount(
+      <Popover shown={true} placement={"right"} />
+    );
+
+    expect(wrapper.find(Popper).prop("placement")).toBe("right");
+  });
+
+  it("should render children as the reference element", () => {
+    const expectedChildElement = <div className={"test-target-element"}/>;
+    const wrapper = mount(
+      <Popover shown={false} >
+        {expectedChildElement}
+      </Popover>
+    );
+
+    expect(wrapper.find(".vs-popover-reference").find(".test-target-element")).toHaveLength(1);
+    expect(wrapper.find(".vs-popover-reference").children().getElement()).toEqual(expectedChildElement);
+  });
+
+  it("should render content as the popover content", () => {
+    const expectedContentElement = <div className={"test-target-content-element"}/>;
+    const wrapper = mount(
+      <Popover shown={false} content={expectedContentElement}>
+        <div className={"test-target-element"}/>
+      </Popover>
+    );
+
+    expect(wrapper.find(".vs-popover-container").find(".test-target-content-element")).toHaveLength(1);
+    expect(wrapper.find(".vs-popover-container").children().at(0).getElement()).toEqual(expectedContentElement);
   });
 });
